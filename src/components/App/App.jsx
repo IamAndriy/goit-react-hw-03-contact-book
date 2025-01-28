00bdbdcc4569581c86f0ba6a60ec6e3405bce6b2
IamAndriy <iamAndriy80@gmail.com>
2025-01-28 14:51:08 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,28 +5,28 @@ import { SectionContactForm } from "../SectionContactForm/SectionContactForm";
 import { SectionContacts } from "../SectionContacts/SectionContacts";
 import css from "./App.module.css";
 
-const contacts = [{id: 'id-1', name: 'Rosie Simpson',  number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements',  number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    {id: 'id-5', name: 'Hermione Kline1', number: '443-89-12'},
-    {id: 'id-6', name: 'Eden Clements2',  number: '645-17-79'},
-    {id: 'id-7', name: 'Annie Copeland1', number: '227-91-26'},
-    {id: 'id-8', name: 'Hermione Kline2', number: '443-89-12'},
-    {id: 'id-9', name: 'Eden Clements2',  number: '645-17-79'},
-    {id: 'id-10', name: 'Annie Copeland2', number: '227-91-26'},
-    {id: 'id-11', name: 'Hermione Kline3', number: '443-89-12'},
-    {id: 'id-12', name: 'Eden Clements3',  number: '645-17-79'},
-    {id: 'id-13', name: 'Annie Copeland3', number: '227-91-26'},
-    {id: 'id-14', name: 'Eden Clements4',  number: '645-17-79'},
-    {id: 'id-15', name: 'Annie Copeland4', number: '227-91-26'},
-    {id: 'id-16', name: 'Hermione Kline5', number: '443-89-12'},
-    {id: 'id-17', name: 'Eden Clements5',  number: '645-17-79'},
-    {id: 'id-18', name: 'Annie Copeland5', number: '227-91-26'},
-    {id: 'id-19', name: 'Hermione Kline56', number: '443-89-12'},
-    {id: 'id-20', name: 'Eden Clements56',  number: '645-17-79'},];
+// const contacts = [{id: 'id-1', name: 'Rosie Simpson',  number: '459-12-56'},
+//     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+//     {id: 'id-3', name: 'Eden Clements',  number: '645-17-79'},
+//     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+//     {id: 'id-5', name: 'Hermione Kline1', number: '443-89-12'},
+//     {id: 'id-6', name: 'Eden Clements2',  number: '645-17-79'},
+//     {id: 'id-7', name: 'Annie Copeland1', number: '227-91-26'},
+//     {id: 'id-8', name: 'Hermione Kline2', number: '443-89-12'},
+//     {id: 'id-9', name: 'Eden Clements2',  number: '645-17-79'},
+//     {id: 'id-10', name: 'Annie Copeland2', number: '227-91-26'},
+//     {id: 'id-11', name: 'Hermione Kline3', number: '443-89-12'},
+//     {id: 'id-12', name: 'Eden Clements3',  number: '645-17-79'},
+//     {id: 'id-13', name: 'Annie Copeland3', number: '227-91-26'},
+//     {id: 'id-14', name: 'Eden Clements4',  number: '645-17-79'},
+//     {id: 'id-15', name: 'Annie Copeland4', number: '227-91-26'},
+//     {id: 'id-16', name: 'Hermione Kline5', number: '443-89-12'},
+//     {id: 'id-17', name: 'Eden Clements5',  number: '645-17-79'},
+//     {id: 'id-18', name: 'Annie Copeland5', number: '227-91-26'},
+//     {id: 'id-19', name: 'Hermione Kline56', number: '443-89-12'},
+//     {id: 'id-20', name: 'Eden Clements56',  number: '645-17-79'},];
 
-localStorage.setItem("CONTACTS", JSON.stringify(contacts));
+// localStorage.setItem("CONTACTS", JSON.stringify(contacts));
 
 const key = "CONTACTS";
 
